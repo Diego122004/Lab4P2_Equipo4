b46d66b38395276cac9935c2d8a1dc5831c3fb40
unknown <luisvarelaromero31@gmail.com>
2023-11-03 14:14:35 -0600

    hola

--- src/lab4p2_equipo4/Equipo.java
+++ src/lab4p2_equipo4/Equipo.java
@@ -9,5 +9,71 @@ package lab4p2_equipo4;
  * @author 50432
  */
 public class Equipo {
-    
+
+    protected String nombre = "";
+    protected String deporte = "";
+    protected int punto = 0;
+    protected int partidos_jugados;
+    protected int ganados=0;
+    protected int perdidos=0;
+
+    public Equipo(String nombre, String deporte, int punto, int partidos_jugados) {
+        this.nombre = nombre;
+        this.deporte = deporte;
+        this.punto = punto;
+        this.partidos_jugados = partidos_jugados;
+   
+    }
+
+    public Equipo() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public int getPunto() {
+        return punto;
+    }
+
+    public void setPunto(int punto) {
+        this.punto = punto;
+    }
+
+    public int getPartidos_jugados() {
+        return partidos_jugados;
+    }
+
+    public void setPartidos_jugados(int partidos_jugados) {
+        this.partidos_jugados = partidos_jugados;
+    }
+
+    public int getGanados() {
+        return ganados;
+    }
+
+    public void setGanados(int ganados) {
+        this.ganados = ganados;
+    }
+
+    public int getPerdidos() {
+        return perdidos;
+    }
+
+    public void setPerdidos(int perdidos) {
+        this.perdidos = perdidos;
+    }
+
 }
---
+++ src/lab4p2_equipo4/Equipo_futbol.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2_equipo4;
+
+/**
+ *
+ * @author Luis Andres Varela
+ */
+public class Equipo_futbol {
+
+    private int ganados;
+    private int perdidos;
+    private 
+}
